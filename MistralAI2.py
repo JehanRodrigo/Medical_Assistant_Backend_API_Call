f93e3adb87cde3ac1c959ae9df36a9dc0fcd8d5e
Ashan Lakshitha <143149942+AshanLakshitha16763@users.noreply.github.com>
2025-01-08 10:58:25 +0530

    prompt modification

--- MistralAI2.py
+++ MistralAI2.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from mistralai import Mistral
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-#load_dotenv()
+load_dotenv()
 
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  
 api_key = os.environ["MISTRAL_API_KEY"]
 print(api_key)
 
@@ -22,7 +22,7 @@ def generate_ai_suggestions(input_text, num_suggestions=3):
         completion = client.chat.complete(
             model="mistral-large-latest",  
             messages=[
-                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user. for example, if the user says 'How' you can generate the text like 'How are you?', 'How is your day going?','How old are you' etc."},
+                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user. for example, if the user says 'How' you can generate the text like 'How are you?', 'How is your day going?','How old are you'. Do not include '\n'. if user input 'how' you must give the full completion like,'how are you','how old are you'. Also make sure you can't give completion like 'are you','old are you' without 'how'. give only one completion within one string. Do not give completion like this,'where do you live?,where were you heading this afternoon?'.Do not give completions,'Sure, here's a completion for','I guess There are many concention with prefix ','school, schedule given What are somerazy fst'. Do not give same completions multiple times. Do not says things like 'Sure, here's a completion for you', 'Sure, here is a completion starting with','Sure, here is a completion for your input'. "},
                 {"role": "user", "content": input_text}
             ],
             max_tokens=12,  
